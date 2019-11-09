84017bfaf3b2d19990b591dee9445bd13b88658e
sean <shonshon7@gmail.com>
2019-11-09 09:41:32 +0800

    Initial Commit

---
+++ Access_App_Exercise_Sean/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Access_App_Exercise_Sean
+//
+//  Created by KimuraSean on 2019/11/9.
+//  Copyright © 2019 sean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
