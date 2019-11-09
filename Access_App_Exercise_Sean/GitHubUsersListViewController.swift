18ad18bcad7f7f03498eb82dd426426ee68c774b
sean <shonshon7@gmail.com>
2019-11-09 10:35:42 +0800

    [feat] add GitHubUsersListCell

--- Access_App_Exercise_Sean/GitHubUsersListViewController.swift
+++ Access_App_Exercise_Sean/GitHubUsersListViewController.swift
@@ -1,14 +1,14 @@
 //
-//  ViewController.swift
+//  GitHubUsersListViewController.swift
 //  Access_App_Exercise_Sean
 //
-//  Created by KimuraSean on 2019/11/9.
+//  Created by sean on 2019/11/9.
 //  Copyright © 2019 sean. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class GitHubUsersListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
