83afbe9c2e1884d13d3e1efea0e92ddb432a4dbd
sean <shonshon7@gmail.com>
2019-11-09 10:50:59 +0800

    [feat] add GitHubUsersListCellViewModel & GitHubUsersListViewModel

---
+++ Access_App_Exercise_Sean/View Model/GitHubUsersListViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  GitHubUsersListViewModel.swift
+//  Access_App_Exercise_Sean
+//
+//  Created by KimuraSean on 2019/11/9.
+//  Copyright © 2019 sean. All rights reserved.
+//
+
+import Foundation
+
+class GitHubUsersListViewModel {
+    
+    var users: [GitHubUser]?
+    
+    func fetchAllUsers() {
+        
+    }
+}
