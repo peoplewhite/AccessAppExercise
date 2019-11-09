f71290dc41b4a421d9cb8bb51935898f9ffa67cf
sean <shonshon7@gmail.com>
2019-11-09 10:39:08 +0800

    [feat] add GitHubUser model

---
+++ Access_App_Exercise_Sean/GitHubUser.swift
@@ -0,0 +1,15 @@
+//
+//  GitHubUser.swift
+//  Access_App_Exercise_Sean
+//
+//  Created by KimuraSean on 2019/11/9.
+//  Copyright © 2019 sean. All rights reserved.
+//
+
+import Foundation
+
+struct GitHubUser {
+    let avatarUrl: String
+    let name: String
+    let jobTitle: String?
+}
