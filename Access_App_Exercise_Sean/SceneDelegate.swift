7353b96c0905557a95798c6267e1ab9ccd959e98
sean <shonshon7@gmail.com>
2024-08-14 23:41:34 +0800

    [feat] setup sceneDelegate to show swiftui

--- Access_App_Exercise_Sean/SceneDelegate.swift
+++ Access_App_Exercise_Sean/SceneDelegate.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let userListView = GitHubUsersListView()
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UIHostingController(rootView: userListView)
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ Access_App_Exercise_Sean/View/GitHubUsersListView.swift
@@ -0,0 +1,19 @@
+//
+//  GitHubUsersListView.swift
+//  Access_App_Exercise_Sean
+//
+//  Created by Sean on 2024/8/14.
+//  Copyright © 2024 sean. All rights reserved.
+//
+
+import SwiftUI
+
+struct GitHubUsersListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GitHubUsersListView()
+}
